269a2c7105930bb6335b7a420920485f978ea66d
Vedang2002 <96991130+Vedang2002@users.noreply.github.com>
2024-02-15 17:21:26 +0530

    Update plot2.R

--- exploratory data analysis/plot2.R
+++ exploratory data analysis/plot2.R
@@ -1,1 +1,8 @@
-
+# Setting language for days axis
+Sys.setlocale("LC_TIME", "English")
+# Adjusting time vector
+date_converted <- as.Date(data[, 1], format = "%d/%m/%Y")
+time_converted <- strptime(data[, 2], format = "%H:%M:%S")
+time_axis <- as.POSIXct(paste(date_converted, data[, 2]))
+#plotting
+plot(time_axis, data[, 3], xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
